21fab7b4cb34e1618604191e587ef8b44ab78cfe
Kimbsy <lordkimber@gmail.com>
2018-08-20 20:02:10 +0100

    initial commit

---
+++ src/main/java/com/kimbsy/tamagotchi/Main.java
@@ -0,0 +1,10 @@
+package com.kimbsy.tamagotchi;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TamagotchiApp tamagotchiApp = new TamagotchiApp();
+        tamagotchiApp.init();
+        tamagotchiApp.run();
+    }
+}
---
+++ src/main/java/com/kimbsy/tamagotchi/TamagotchiApp.java
@@ -0,0 +1,21 @@
+package com.kimbsy.tamagotchi;
+
+/**
+ * Main application class.
+ *
+ * @author kimbsy
+ */
+public class TamagotchiApp implements Runnable{
+
+    TamagotchiApp() {
+
+    }
+
+    public void init() {
+
+    }
+
+    public void run() {
+
+    }
+}
